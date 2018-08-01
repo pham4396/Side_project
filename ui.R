5be78739a34d217d8fb248f633a0324f9a1417c1
Douglas Pham <pham4396@uw.edu>
2018-08-01 15:27:08 -0700

    changed boxes to radio buttons, got audition, conference, and members filters to work together

--- ui.R
+++ ui.R
@@ -24,17 +24,17 @@ my_ui <- fluidPage(
         sliderInput("members_filter", label = "How many members?",
                      min = min(university_bands_without_websites$`Number of Members`),
                      max = max(university_bands_without_websites$`Number of Members`),
-                     value = c(median(university_bands_without_websites$`Number of Members`) - 20,
-                               median(university_bands_without_websites$`Number of Members`) + 20 )
+                     value = c(min(university_bands_without_websites$`Number of Members`), 
+                               max(university_bands_without_websites$`Number of Members`))
         ),
-        checkboxGroupInput("colorguard_filter", label = "Would you like to see bands with a color Guard?",
+        radioButtons("colorguard_filter", label = "Would you like to see bands with a color Guard?",
                            choices = c("Yes, Only bands with a Color Guard", "No, All Bands", "No, Only bands without a Color Guard")
         
         ),
         radioButtons("audition_filter", label = "Auditions?", 
                           choices = unique(university_bands_without_websites$`Auditions?`)
         ),
-        checkboxGroupInput("style_filter", label = "Which marching Style would you like to look at",
+        radioButtons("style_filter", label = "Which marching Style would you like to look at",
                            choices = c("Chair Step", "Ankle Knee Step", "Straight Leg", "Roll Step", "Glide Step", "Scatter", "Corps Style")
         ),
         uiOutput("conference_filters")
@@ -49,7 +49,7 @@ my_ui <- fluidPage(
                 Their Leg till their thigh is parallel to the ground and their calf is perpindicular to the
                 ground while also point their toes towards the ground. At this point the leg looks like a 'chair'."),
                 p("Straight Leg: A very clean marching Style where the leg does not bend. when a band marches with this style
-                  it will look like bands legs are sciccors cut the air."),
+                  it will look like its member's legs are sciccors cutting the air."),
                 p("Glide/Roll Step : The most common form of marching at both the high school and collegiate level, Glide/Roll step
                   minimizes movment of the upper body. The feet 'roll' when marching absorbing schock during movement. Most bands march
                   chair step during their pre-game show, and use Roll/Glide for their halftime show, though some bands use chair step for both"))
@@ -63,8 +63,11 @@ my_server <- function(input, output, session ){
   
   band_filters <- reactive({
     bands <- university_bands_without_websites %>%
-      filter(input$conference_filters == Conference,
-            input$audition_filter == `Auditions?`
+      filter(
+        input$conference_filters == Conference &
+            input$audition_filter == `Auditions?` & 
+               `Number of Members` >= input$members_filter [1] &
+               `Number of Members` <= input$members_filter [2]
       )
     return(bands)
   })
